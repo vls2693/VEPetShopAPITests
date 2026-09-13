92a17c662433ed64a8b3c5b7c8617e2560e40438
beliakov <beliakov@tochka.com>
2026-09-13 21:34:39 +0200

    тест "Получение списка питомцев по статусу"

--- test/test_pet.py
+++ test/test_pet.py
@@ -1,5 +1,6 @@
 import allure
 import jsonschema
+import pytest
 import requests
 from .schemas.pet_schema import PET_SCHEMA
 
@@ -151,3 +152,22 @@ class TestPet:
         with allure.step("Проверка статуса ответа и данных питомца"):
             assert response.status_code == 200
             assert response.json().get("id") == pet_id
+
+    @allure.title("Получение списка питомцев по статусу")
+    @pytest.mark.parametrize(
+        "status, expected_status_code",
+            [
+                ("available", 200),
+                ("sold", 200),
+                ("pending", 200),
+                ("qqq", 400),
+                ("", 400)
+            ]
+        )
+    def test_get_pets_by_status(self, status, expected_status_code):
+        with allure.step(f"Отправка запроса на получение питомцев по статусу"):
+            response = requests.get(f"{BASE_URL}/pet/findByStatus", params={"status": "sold"})
+
+        with allure.step("Проверка статуса ответа и формата данных"):
+            assert response.status_code == 200
+            assert isinstance(response.json(), list)
